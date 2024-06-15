452a4ef08673442906348d71c9c670814e7bf9e8
GustavoHC <u202213983@upc.edu.pe>
2024-06-15 16:00:35 -0500

    fix: mongodb server configs

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const dbconnect = () => {
-    mongoose.connect("mongodb://127.0.0.1:27017/UserDbExample", {useNewUrlParser:true})
+    mongoose.connect("mongodb://mongo:27017/test", {useNewUrlParser:true, useUnifiedTopology: true})
     .then(() =>{
          console.log('Database connected')
     })
